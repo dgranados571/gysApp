6fa34c747e2d62ea0f80362a61abbd4dd9f4113f
Danny Granados <dgranados571@gmail.com>
2022-05-28 17:08:39 -0500

    Actializa componente entero

--- src/Components/Admin/ListaEquipos.js
+++ src/Components/Admin/ListaEquipos.js
@@ -3,7 +3,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCirclePlus } from '@fortawesome/free-solid-svg-icons'
 
 import imagen1 from '../../img/panelesSolaresSol.jpg'
-import imagen2 from '../../img/PanelesSolares.jpg'
+import imagen2 from '../../img/panelesSolares.jpg'
 import imagen3 from '../../img/mantenimiento.jpg'
 import { Paginacion } from '../../Paginacion'
 
@@ -19,12 +19,12 @@ export const ListaEquipos = ({ setControlView }) => {
     }
 
     const [elementsList, setElementsList] = useState([
-        { id: '1', urlImage: imagen3, nombre: 'Maquina 1', ubicacion: 'Linea 1' },
-        { id: '2', urlImage: imagen3, nombre: 'Maquina 2', ubicacion: 'Linea 1' },
+        { id: '1', urlImage: imagen1, nombre: 'Maquina 1', ubicacion: 'Linea 1' },
+        { id: '2', urlImage: imagen2, nombre: 'Maquina 2', ubicacion: 'Linea 1' },
         { id: '3', urlImage: imagen3, nombre: 'Maquina 3', ubicacion: 'Linea 2' },
-        { id: '4', urlImage: imagen3, nombre: 'Maquina 4', ubicacion: 'Linea 2' },
+        { id: '4', urlImage: imagen1, nombre: 'Maquina 4', ubicacion: 'Linea 2' },
         { id: '5', urlImage: imagen3, nombre: 'Maquina 5', ubicacion: 'Linea 3' },
-        { id: '6', urlImage: imagen3, nombre: 'Maquina 6', ubicacion: 'Linea 3' }
+        { id: '6', urlImage: imagen2, nombre: 'Maquina 6', ubicacion: 'Linea 3' }
     ])
 
     return (
